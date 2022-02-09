1f8fd1608b7360d464903945ae01c09d5e9ab30d
Gusthawo Junkes <gusthawojunkes14@gmail.com>
2022-02-08 21:22:56 -0300

    npm run lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,15 +9,27 @@ module.exports = {
         requireConfigFile: false,
         ecmaVersion: 2020,
     },
-    extends: ['plugin:vue/essential', '@vue/airbnb'], //'prettier'
+    extends: ['plugin:vue/essential', '@vue/airbnb'], // 'prettier'
     parser: 'vue-eslint-parser',
     plugins: [],
     rules: {
+        'linebreak-style': [
+            'error',
+            process.platform === 'win32' ? 'windows' : 'unix',
+        ],
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         semi: ['error', 'always'],
         quotes: ['error', 'single'],
         indent: ['error', 4],
         'comma-spacing': ['error', { before: false, after: true }],
+        'import/no-extraneous-dependencies': [
+            'error',
+            {
+                devDependencies: false,
+                optionalDependencies: false,
+                peerDependencies: false,
+            },
+        ],
     },
 };
--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import Vue from 'vue';
-import App from './App.vue'
-import axios from 'axios';
-//import router from '@/router';
 import vuetify from '@/plugins/vuetify';
+import axios from 'axios';
+import App from './App.vue';
+// import router from '@/router';
 
 Vue.config.productionTip = false;
 Vue.prototype.$http = axios;
 
 new Vue({
-  /*router,*/
-  vuetify,
-  render: (h) => h(App),
+    /* router, */
+    vuetify,
+    render: (h) => h(App),
 }).$mount('#app');
