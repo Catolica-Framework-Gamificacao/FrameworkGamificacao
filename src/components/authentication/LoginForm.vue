648dbeb88e0bff442176d86c43d34d82d9172696
Gusthawo Junkes <gusthawojunkes14@gmail.com>
2022-03-02 13:30:58 -0300

    WIP: LoginForm

--- src/components/authentication/LoginForm.vue
+++ src/components/authentication/LoginForm.vue
@@ -1,10 +1,48 @@
 <template>
-    <div>Login</div>
+    <v-card
+        class="mx-auto rounded-xl"
+        opacity="1"
+        dark
+        max-width="50%"
+        elevation="24"
+    >
+        <v-card-title>
+            <span class="text-h6 font-weight-light">Log in</span>
+        </v-card-title>
+        <v-card-text>
+            <v-row>
+                <v-col cols="12">
+                    <v-text-field
+                        v-model="form.login"
+                        label="E-mail"
+                    ></v-text-field>
+                </v-col>
+                <v-col cols="12">
+                    <v-text-field
+                        v-model="form.password"
+                        :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                        type="password"
+                        label="Senha"
+                        @click:append="showPassword = !showPassword"
+                    ></v-text-field>
+                </v-col>
+            </v-row>
+        </v-card-text>
+        <v-card-actions>
+            <v-btn text block> Entrar </v-btn>
+        </v-card-actions>
+    </v-card>
 </template>
 
 <script>
 export default {
     name: 'LoginForm',
-    data: () => ({}),
+    data: () => ({
+        showPassword: false,
+        form: {
+            login: '',
+            password: '',
+        },
+    }),
 };
 </script>
