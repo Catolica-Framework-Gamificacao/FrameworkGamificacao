bf4182dce4cb079b0fe602ce69d77343e65b69e8
Gusthawo Junkes <gusthawojunkes14@gmail.com>
2022-03-10 13:26:37 -0300

    RankingService.loadData()

--- src/services/RankingService.js
+++ src/services/RankingService.js
@@ -36,6 +36,30 @@ export default class RankingService {
             },
         ];
     }
+
+    static async loadData() {
+        let ranking = [];
+        const service = this;
+
+        ranking = await axios
+            .get(service.PATH)
+            .then((response) => {
+                ranking = response;
+                if (ranking.length === 0 || !_.isArray(ranking)) {
+                    return [];
+                }
+
+                ranking.forEach((position) => {
+                    service.prepare(position);
+                });
+
+                return ranking;
+            })
+            .catch((error) => {
+                if (error && error.message) {
+                    console.error(error.message);
+                }
+                return [];
             });
         return ranking;
     }
